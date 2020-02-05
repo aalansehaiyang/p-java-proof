c019f7418f27024070dd9f36b107f40bfc56d267
wenwan <aalansehaiyang52@126.com>
2020-02-05 15:54:04 +0800

    AtomicReference

--- src/main/java/proof/chapter6/AtomicReference1.java
+++ src/main/java/proof/chapter6/AtomicReference1.java
@@ -13,20 +13,20 @@ public class AtomicReference1 {
 
         // 启动1000个线程，每个线程计数+1
         for (int i = 0; i < 1000; i++) {
-            Thread t = new Thread(new Task(ref), "Thread-" + i);
+            Thread t = new Thread(new Task2(ref), "Thread-" + i);
             t.start();
         }
-
+        System.out.println("输出结果：");
         System.out.println(ref.get()); // 打印2000
     }
 
 }
 
-class Task implements Runnable {
+class Task1 implements Runnable {
 
     private AtomicReference<Integer> ref;
 
-    Task(AtomicReference<Integer> ref){
+    Task1(AtomicReference<Integer> ref){
         this.ref = ref;
     }
 
@@ -37,5 +37,24 @@ class Task implements Runnable {
             if (ref.compareAndSet(oldV, oldV + 1)) // CAS操作
                 break;
         }
+    }
+}
+
+class Task2 implements Runnable {
+
+    private AtomicReference<Integer> ref;
+
+    Task2(AtomicReference<Integer> ref){
+        this.ref = ref;
+    }
+
+    @Override
+    public void run() {
+//        try {
+//            Thread.sleep(10);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        ref.updateAndGet(t -> t + 1);
     }
 }
